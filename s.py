45554f53255a6b1e5efe93b70c6f0d0dbe2740a9
Calvin Quach <calvin@blocpower.org>
2017-07-10 18:48:46 -0400

    Server: Decode user coords sent

--- s.py
+++ s.py
@@ -8,11 +8,15 @@ class myRequestHandler(socketserver.BaseRequestHandler):
 		# self.request is the TCP socket connected to the client
 		#retrieve player command
 		self.data = self.request.recv(1024).strip()
+		coords = self.data.decode("utf-8").split(',')
+		coords = [int(i) for i in coords]
+		print(coords)
 
 		print("{} wrote:".format(self.client_address[0]))
 		print(self.data)
 
 		#TODO: update gameboard
+		success = game.setPiece(coords)
 
 		#TODO: make computer move
 
