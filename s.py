735bc1e6da637f6128db75373cbfb1a689d17682
Calvin Quach <calvin@blocpower.org>
2017-07-11 18:28:57 -0400

    Keep connection by wrapping client socket in loop until winner is found.

--- s.py
+++ s.py
@@ -9,36 +9,52 @@ class myRequestHandler(socketserver.BaseRequestHandler):
 	def handle(self):
 		# self.request is the TCP socket connected to the client
 		#retrieve player command
-		self.data = self.request.recv(1024).strip()
-		coords = self.data.decode("utf-8").split(',')
-		coords = [int(i) for i in coords]
-		print(coords)
+		while(True):
+			self.data = self.request.recv(1024).strip()
+			coords = self.data.decode("utf-8").split(',')
+			coords = [int(i) for i in coords]
+			print("Client: ",coords)
 
-		print("{} wrote:".format(self.client_address[0]))
-		print(self.data)
+			print("{} wrote:".format(self.client_address[0]))
+			print(self.data)
 
-		# set piece
-		success = game.setPiece(coords)
-		#if not good move, prompt user for another coord and end
-		if not success:
-			message = pickle.dumps('Failed: Try Again')
-			self.request.sendall(message)
-			return
-
-		#TODO: check winner
-		
-
-		#make computer move
-		success = False
-		while(not success):
-			x = random.randint(0,2)
-			y = random.randint(0,2)
-			coords = [x,y]
+			# set piece
 			success = game.setPiece(coords)
 
-		#send gameboard
-		data = game.export()
-		self.request.sendall(data)
+			#if not good move, prompt user for another coord and end
+			if not success:
+				message = pickle.dumps('Failed: Try Again')
+				self.request.sendall(message)
+				return
+
+			#check winner (TODO: make into method)
+			winner = game.checkWinner()
+			if(winner != 0):
+				data = game.exportAll()
+				self.request.sendall(data)
+				break
+
+			print(game)
+			#make computer move (TODO: make into method)
+			success = False
+			while(not success):
+				my_coords = input('>>')
+				print(my_coords)
+				my_coords = my_coords.split(',')
+				my_coords = [int(i) for i in my_coords]
+				print("Server: ",my_coords)
+				success = game.setPiece(my_coords)
+
+			#check winner (TODO: make into method)
+			winner = game.checkWinner()
+			if(winner != 0):
+				data = game.exportAll()
+				self.request.sendall(data)
+				break
+
+			#send gameboard
+			data = game.exportAll()
+			self.request.sendall(data)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
